1cd7944a79078385f293a5430a706deca00cf297
PasinduAnjana <pasinduanjana000@gmail.com>
2023-09-29 18:12:52 +0530

    added multiple instances

--- gamelogic.js
+++ gamelogic.js
@@ -3,16 +3,29 @@ import { importCrop1, importFlower1 } from './3dObjects';
 import { gsap } from 'gsap';
 import * as SkeletonUtils from 'three/examples/jsm/utils/SkeletonUtils';
 
+
+
 let ranRot=0;
 let crop1;
 let flower1;
+let activeAni;
 let flower1ani;
+let crop1Ani;
 let cropCount=0;
 var cropCountElement=document.getElementById("count");
+let activeCrop;
+
+
+var CropBtn = document.getElementById("cropSel");
+var FlowerBtn = document.getElementById("flowerSel");
+
+clicCrop();
+CropBtn.addEventListener('click',clicCrop );
+FlowerBtn.addEventListener('click',clicFlower );
 
 importCrop1((crop,animations)=>{
     crop.position.x=3;
-    flower1ani=animations[0];
+    crop1Ani=0;
     crop1=crop;
     //console.log(crop1);
 });
@@ -21,24 +34,41 @@ importFlower1((flower,animations)=>{
     flower.position.x=2;
     flower1ani=animations[0];
     flower1=flower;
-
   }    
 );
 
 
+function clicCrop(){
+    activeCrop=crop1;
+    activeAni=crop1Ani;
+}
+
+function clicFlower(){
+  activeCrop=flower1;
+    activeAni=flower1ani;
+}
+
+
 export function spawnOnClick(highlight,scene,clonedObjs,mixers){
+
+  if(!activeCrop){
+    clicCrop();
+  }
+    
     //const sphereClone=crop1.clone();
-    const sphereClone=SkeletonUtils.clone(flower1);
+    const sphereClone=SkeletonUtils.clone(activeCrop);
           sphereClone.position.copy(highlight.position);
           sphereClone.rotation.y=ranRot;
           sphereClone.scale.set(.1,.1,.1)
           scene.add(sphereClone);
 
           //animation
-          const mixer=new THREE.AnimationMixer(sphereClone);
-          const clip= mixer.clipAction(flower1ani);
-          clip.play();
-          mixers.push(mixer);
+          if(activeAni){
+            const mixer=new THREE.AnimationMixer(sphereClone);
+            const clip= mixer.clipAction(activeAni);
+            clip.play();
+            mixers.push(mixer);
+          }
           
           
 
@@ -61,14 +91,14 @@ export function clearObjects(scene,clonedObjs){
 
 
     if (crop.children[0] instanceof THREE.Mesh) {
-
+      console.log('fst');
       scene.remove(crop);  
       crop.children[0].geometry.dispose(); 
       crop.children[0].material.dispose();
 
     }
     if (crop.children[0].children[0] instanceof THREE.Mesh) {
-
+      console.log('snd');
       scene.remove(crop);  
       crop.children[0].children[0].geometry.dispose(); 
       crop.children[0].children[0].material.dispose();
